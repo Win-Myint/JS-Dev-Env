cf1a88830df6520e4f74514ba4d9491eea09c791
winniefer <win.m179@gmail.com>
2017-12-29 11:43:12 +0000

    added eslint with recommended rules to enforce project consistency and catch error early

--- src/index.js
+++ src/index.js
@@ -1,5 +1,3 @@
 import './index.css'
-import numeral from 'numeral'
 
-const courseValue = numeral(1000).format('$0,0.00')
-console.log(`I would pay ${courseValue} for this course!`)
+alert('hello world')
